2b3164caec6178e7765b608c42f6ed89a431f63d
gjergjk71 <gjergjk71@gmail.com>
2018-07-27 17:43:30 +0200

    "/register" post handles unique username error

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const Database = require('better-sqlite3');
 const db = new Database('./db.sqlite3');
 const saltRounds = 10;
 
+function uniqueUsername(username) {
+	users = db.prepare("SELECT * FROM User").all();
+	for (user in users) {
+		if (users[user].username === username) {
+			console.log(`${users[user].username} === ${username}`);
+			return false;
+		} else {
+			console.log(`${users[user].username} !== ${username}`);
+		}
+	};
+	return true;
+};
+
 db.prepare("CREATE TABLE IF NOT EXISTS User ( \
 			id INTEGER PRIMARY KEY,\
 			name TEXT,\
@@ -27,19 +40,23 @@ app.post("", function(req,res){
 	});
 
 app.get("/register", function(req,res){
-		res.render("pages/register");
+		res.render("pages/register",{error:""});
 	});
 
 app.post("/register", function(req,res){
 		var name = req.body.name;
 		var username = req.body.username;
 		var password = req.body.password;
-		bcrypt.hash(password, saltRounds, function(err, hash) {
-  				db.prepare(`INSERT INTO User (name,username,password) VALUES ('${name}','${username}','${hash}');`).run();
-			});
-		res.redirect("/");
+		if (uniqueUsername(username)){
+			bcrypt.hash(password, saltRounds, function(err, hash) {
+	  				db.prepare(`INSERT INTO User (name,username,password) VALUES ('${name}','${username}','${hash}');`).run();
+				});
+			res.redirect("/");
+		} else {
+			res.render("pages/register",{error:"Username is already taken."});
+		}
 		res.end();
 	});
 
-app.listen(8080);
+app.listen(8000);
 //		db.prepare(`INSERT INTO User (name,username,password) VALUES (${name},${username},${password});`).run();
