e3168c22262c5dd904cdf04780101098c154647d
gjergjk71 <gjergjk71@gmail.com>
2018-07-27 16:40:20 +0200

    Implemented register

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bcrypt = require("bcrypt")
+const bodyParser = require("body-parser");
+const app = express();
+const Database = require('better-sqlite3');
+const db = new Database('./db.sqlite3');
+const saltRounds = 10;
 
+db.prepare("CREATE TABLE IF NOT EXISTS User ( \
+			id INTEGER PRIMARY KEY,\
+			name TEXT,\
+			username TEXT UNIQUE,\
+			password TEXT\
+	);").run();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("", function(req, res){
-		res.render("pages/index",{
+		res.render("pages/login",{
 				foods:["dsa","dsa","DSAA"]
 			});
 	});
 
+app.post("", function(req,res){
+
+	});
+
+app.get("/register", function(req,res){
+		res.render("pages/register");
+	});
+
+app.post("/register", function(req,res){
+		var name = req.body.name;
+		var username = req.body.username;
+		var password = req.body.password;
+		bcrypt.hash(password, saltRounds, function(err, hash) {
+  				db.prepare(`INSERT INTO User (name,username,password) VALUES ('${name}','${username}','${hash}');`).run();
+			});
+		res.redirect("/");
+		res.end();
+	});
 
 app.listen(8080);
+//		db.prepare(`INSERT INTO User (name,username,password) VALUES (${name},${username},${password});`).run();
